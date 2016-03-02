96dda360f6f54be214e4eb564e242edaa299359d
garnesen <garnesen@vt.edu>
2016-03-01 22:05:31 -0500

    Store edges visited

--- src/math/PrimsAlgorithm.java
+++ src/math/PrimsAlgorithm.java
@@ -1,5 +1,6 @@
 package math;
 
+import java.awt.Point;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,29 +15,33 @@ import utils.Utils;
  *
  */
 public class PrimsAlgorithm extends Graph {
-	
+
 	/**
 	 * Create a WeightedGraph from a SquareMatrix (adjacency matrix).
 	 * @param am
 	 */
 	public PrimsAlgorithm(SquareMatrix am) {
 		super(am);
+		edgesVisited = new ArrayList<Point>();
 		verticesVisited = new ArrayList<Integer>();
 		currentWeight = 0;
 	}
-	
+
 	/**
 	 * Creates a WeightedGraph from the string representation of it.
 	 * @param amRepresentation
 	 */
 	public PrimsAlgorithm(String amRepresentation) {
 		super(amRepresentation);
+		edgesVisited = new ArrayList<Point>();
 		verticesVisited = new ArrayList<Integer>();
 		currentWeight = 0;
 	}
-	
+
+	private List<Point> edgesVisited; 
 	private List<Integer> verticesVisited;
 	private int currentWeight;
+	
 	/**
 	 * Steps forward once in Prim's algorithm. This is defined by either getting
 	 * a random vertex if this is the first step or adding a vertex adjacent to
@@ -49,7 +54,7 @@ public class PrimsAlgorithm extends Graph {
 			Utils.log("Prim", "Total weight of minimal spanning tree: " + currentWeight);
 			return false;
 		}
-		
+
 		// The algorithm is taking the first step, pick a random vertex.
 		if (verticesVisited.size() == 0) {
 			int vertex = (int) (Math.random() * this.getNumberVertices());
@@ -57,7 +62,7 @@ public class PrimsAlgorithm extends Graph {
 			Utils.log("Prim", "Added random vertex " + (vertex + 1));
 			return true;
 		}
-		
+
 		// Pick a least-expensive edge with one vertex in the array and one not.
 		int minEdge = Integer.MAX_VALUE;
 		int curVertex = -1;
@@ -73,16 +78,17 @@ public class PrimsAlgorithm extends Graph {
 				}
 			}
 		}
+		edgesVisited.add(new Point(curVertex, newVertex));
 		verticesVisited.add(newVertex);
 		currentWeight += minEdge;
-		
+
 		// Log the data
 		StringBuilder sb = new StringBuilder("Using edge ").append(curVertex + 1).append("-").append(newVertex + 1).append(", with weight ").append(minEdge);
 		Utils.log("Prim", sb.toString());
-		
+
 		return true;
 	}
-	
+
 	/**
 	 * Steps backwards in Prim's algorithm. This is done by removing the vertex
 	 * last visited.
@@ -94,22 +100,27 @@ public class PrimsAlgorithm extends Graph {
 			Utils.log("Prim", "Cannot step back more");
 			return false;
 		}
-		
+
 		// Remove the last step made.
 		int vertex = verticesVisited.remove(verticesVisited.size() - 1);
-		
-		// Log the data
-		Utils.log("Prim", "Stepped back, removed vertex " + vertex);
+		if (edgesVisited.size() > 0) {
+			Point edge = edgesVisited.remove(edgesVisited.size() - 1);
+			currentWeight -= getAdjacencyMatrix().get(edge.x, edge.y);
+			Utils.log("Prim", "Removed vertex " + (vertex+1) + " and edge " + (edge.x+1) + "-" + (edge.y+1));
+		} else {
+			Utils.log("Prim", "Removed vertex " + (vertex+1));
+		}
+
 		return true;
 	}
-	
+
 	/**
 	 * Resets the algorithm by clearing the list.
 	 */
 	public void reset() {
 		verticesVisited.clear();
 	}
-	
+
 	/**
 	 * Gets a list of vertices that have been visited.
 	 * @return the list of visited vertices
@@ -117,5 +128,22 @@ public class PrimsAlgorithm extends Graph {
 	public List<Integer> getVerticesVisited() {
 		return verticesVisited;
 	}
-	
+
+	/**
+	 * Get a list of edges that have been visited.
+	 * @return the list of visited edges
+	 */
+	public List<Point> getEdgesVisited() {
+		return edgesVisited;
+	}
+
+	/**
+	 * Check if a given edge has been visited.
+	 * @param edge
+	 * @return
+	 */
+	public boolean seenEdge(Point edge) {
+		return edgesVisited.stream().anyMatch(e -> (e.x == edge.x && e.y == edge.y) || (e.x == edge.y && e.y == edge.x));
+	}
+
 }
